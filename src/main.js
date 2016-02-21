68806ad5418555cbc38a77972011977f52b2fe33
ewgenius <ewgeniux@gmail.com>
2016-02-21 12:24:40 +0300

    init web server

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+let PORT = process.env.PORT || 3000;
+let app = express();
+
+let server = app.listen(PORT, () => {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log(`server listening at http://${host}:${port}`);
+})
