98d442d36128be3d4645c35fac6587c42a933101
ewgenius <ewgeniux@gmail.com>
2016-02-21 13:00:12 +0300

    socket init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import cors from 'cors';
+import {
+  createServer
+} from 'http';
+import IO from 'socket.io';
+
+const origins = 'http://localhost:8000, https://kirov-bus.firebaseapp.com/';
 
 let PORT = process.env.PORT || 3000;
 let app = express();
+app.use(cors({
+  origin: origins,
+  credentials: true
+}));
 
-let server = app.listen(PORT, () => {
+const server = createServer(app);
+const io = IO(server);
+
+io.set('origins', origins);
+
+io.on('connection', socket => {
+  console.log('connected');
+  socket.emit('connected', 'test');
+});
+
+server.listen(PORT, () => {
   let host = server.address().address;
   let port = server.address().port;
 
