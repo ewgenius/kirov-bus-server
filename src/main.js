7d6d2688137e2fcbd3ddb40731d1cacc1cb3ea37
ewgenius <ewgeniux@gmail.com>
2016-02-25 19:12:18 +0300

    realtime update

--- src/main.js
+++ src/main.js
@@ -42,26 +42,93 @@ io.on('connection', socket => {
   socket.emit('connected', 'test');
 
   socket.on('subscribe', route => {
-    console.log(`http://m.cdsvyatka.com/many_json.php?marsh=${route}`);
     socket.join(`route-${route}`);
-    request(`http://m.cdsvyatka.com/many_json.php?marsh=${route}`)
-      .then(r => {
-        console.log(typeof r, r);
-        return r;
-      })
-      .then(r => JSON.parse(r))
-      .then(result => {
-        io.to(`route-${route}`).emit('route.update', {
-          route: route,
-          data: result
-        });
-      }).catch(err => console.error(err));
   });
 
   socket.on('unsubscribe', route => {
     socket.leave(`route-${route.id}`);
   });
 });
+
+const pool = [
+  1001,
+  1002,
+  1003,
+  1005,
+  1009,
+  1010,
+  1012,
+  1014,
+  1015,
+  1016,
+  1017,
+  1019,
+  1020,
+  1021,
+  1022,
+  1023,
+  1026,
+  1033,
+  1037,
+  1038,
+  1039,
+  1044,
+  1046,
+  1050,
+  1051,
+  1052,
+  1053,
+  1054,
+  1061,
+  1067,
+  1070,
+  1074,
+  1084,
+  1087,
+  1088,
+  1090,
+  3101,
+  3104,
+  3116,
+  3117,
+  3129,
+  3136,
+  3143,
+  3146,
+  5001,
+  5003,
+  5004,
+  5005,
+  5007,
+  5008,
+  5011,
+  5014
+];
+
+function requestRoute(route) {
+  return request(`http://m.cdsvyatka.com/many_json.php?marsh=${route}`)
+    .then(r => JSON.parse(r.trim()));
+}
+
+setInterval(() => {
+  let route = pool.pop();
+  requestRoute(route)
+    .then(result => {
+      console.log(`update for ${route}`);
+      io.to(`route-${route}`).emit('route.update', {
+        route: route,
+        data: result
+      });
+      pool.unshift(route);
+    })
+    .catch(() => {
+      console.log(`retry for ${route}`);
+      pool.push(route);
+    })
+}, 500);
+
+
+
 
 server.listen(PORT, () => {
   let host = server.address().address;
