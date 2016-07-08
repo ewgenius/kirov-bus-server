01ebfec0872f337f04ee5140ca06433c3863f343
ewgenius <ewgeniux@gmail.com>
2016-07-08 22:10:30 +0300

    init ts

--- src/cds.js
+++
@@ -1,6 +0,0 @@
-import request from 'request-promise'
-
-export function getRoute(route) {
-  return request(`http://m.cdsvyatka.com/many_json.php?marsh=${route}`)
-    .then(r => JSON.parse(r.trim()));
-}
--- src/oldcode/routes.js
+++
@@ -1,8 +0,0 @@
-import request from 'request-promise';
-
-export default {
-  getRoute: route => {
-    return request(`http://m.cdsvyatka.com/scheme.php?marsh=${route}`)
-      .then(r => JSON.parse(r))
-  }
-};
