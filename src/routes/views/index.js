a900c234666e0d2e3d0694d3c34f3767c081aa12
ewgenius <ewgeniux@gmail.com>
2016-07-15 16:56:42 +0300

    fresh start

--- src/routes/views/index.js
+++
@@ -1,9 +0,0 @@
-import keystone from 'keystone'
-
-exports = module.exports = function(req, res) {
-	const view = new keystone.View(req, res)
-	const locals = res.locals
-	locals.section = 'home'
-	console.log(req.locale)
-	view.render('index')
-};
