ec9f9196887d3b98d1f051112b1d2cca0107ab67
ewgenius <ewgeniux@gmail.com>
2016-07-15 19:05:25 +0300

    upd cors

--- build/app.js
+++ build/app.js
@@ -1,9 +1,11 @@
 "use strict";
 var express = require('express');
+var cors = require('cors');
 var http_1 = require('http');
 var IO = require('socket.io');
 var api_1 = require('./api/api');
 var PORT = process.env.PORT || 3000;
+var FRONTEND_HOST = process.env.FRONTEND_HOST || 'http://localhost:8080';
 var app = express();
 var server = http_1.createServer(app);
 var io = IO(server);
@@ -12,8 +14,11 @@ cds.on('routeUpdate', function (route, update) {
     console.log(route, update);
     io.to(String(route)).emit('route.update', update);
 });
+app.use(cors({
+    origin: FRONTEND_HOST,
+    credentials: true
+}));
 app.get('/api/v1/route/:route', function (req, res) {
-    console.log(req.params.route);
     cds.getRoute(req.params.route)
         .then(function (result) { return res.send(result); })
         .catch(function (err) { return res.status(500).send(err); });
--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,37 @@
 import * as express from 'express'
+import * as cors from 'cors'
 import {createServer} from 'http'
 import * as IO from 'socket.io'
 import * as mongoose from 'mongoose'
 import {CDS} from './api/api'
 
 const PORT = process.env.PORT || 3000
+const FRONTEND_HOST = process.env.FRONTEND_HOST || 'http://localhost:8080'
 
 const app = express()
 const server = createServer(app)
 const io = IO(server)
 
+// cds api
 const cds = new CDS()
 cds.on('routeUpdate', (route, update) => {
   console.log(route, update)
   io.to(String(route)).emit('route.update', update)
 })
 
+// http server
+app.use(cors({
+  origin: FRONTEND_HOST,
+  credentials: true
+}))
+
 app.get('/api/v1/route/:route', (req, res) => {
-  console.log(req.params.route)
   cds.getRoute(req.params.route)
     .then(result => res.send(result))
     .catch(err => res.status(500).send(err))
 })
 
+// socket connection
 io.on('connection', socket => {
   socket.on('subscribe', route => {
     socket.join(route)
